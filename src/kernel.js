0de68bc406310bb4a953233e3bd460d81cf59e67
Karehb <Karehb@gmail.com>
2023-08-06 18:51:04 +0200

    cambio de efecto Alerta

--- src/kernel.js
+++ src/kernel.js
@@ -33,7 +33,7 @@ function setHeader( msg = " " ) {
     <img align="left" src="config/network/${ serverDatabase.serverAddress }/${ serverDatabase.iconName }" width="400" height="225" style="padding: 0px 0px 0px 0px">
     <h1 style="letter-spacing: 4px">${ serverDatabase.serverName }</h1>
     <p>Conectado a: ${ serverDatabase.serverAddress } ( ${ dateStr } ) </p>
-    <p class='glow' style='font-size: 2em'>ALERTA situación excepcional activada.</p>
+    <p class='desync' style='font-size: 2em'>⚠ ALERTA situación excepcional activada.</p>
     <p>Escribe "help" para más información.</p>
     `;
     // Clear content:
