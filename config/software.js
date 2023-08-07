b90542d8ce436a617fee849f70810e417efe703a
Karehb <Karehb@gmail.com>
2023-08-07 02:58:58 +0200

    intentando agregar puertasx2

--- config/software.js
+++ config/software.js
@@ -26,39 +26,22 @@ function puerta(args) {
     if (args.length === 0 || typeof args[0] !== "number") {
         return [
             "<p>An ID must be provided: <code>puerta $id</code></p>",
-            `You currently have access to the following doors: ${Object.keys(PUERTAS).join(", ")}`
+            `You currently have access to the following puertas: ${Object.keys(PUERTAS).join(" ")}`
         ];
     }
     const puertaId = args[0];
-    const puertaAction = args[1];
-
     const puertaDweet = PUERTAS[puertaId];
     if (!puertaDweet) {
-        return `You do not have access to the door with ID ${puertaId}`;
+        return `You do not have access to the puerta with ID ${puertaId}`;
     }
-
-    if (PUERTAS[puertaId].hasOwnProperty(puertaAction)) {
-        return PUERTAS[puertaId][puertaAction];
-    } else {
-        return `Invalid action for door with ID ${puertaId}. Please specify a valid action.`;
-    }
+    return puertaDweet();
 }
 
 const PUERTAS = {
-    lab: {
-        bloquear: "ID lab bloqueada",
-        desbloquear: "ID lab desbloqueada",
-        contenido: "<p>PUERTA lab</p>"
+    777: function() {
+        return "<p>PUERTA DE SEGURIDAD 888</p>";
     },
-    exit: {
-        bloquear: "ID exit bloqueada",
-        desbloquear: "ID exit desbloqueada",
-        contenido: "<p>PUERTA exit</p>"
-    },
-    // Agrega más IDs y sus contenidos aquí:
-    10550: {
-        bloquear: "ID 10550 bloqueada",
-        desbloquear: "ID 10550 desbloqueada",
-        contenido: "<p>PUERTA 10550</p>"
+    34908: function() {
+        return "<p>PUERTA DE SEGURIDAD 10549</p>";
     },
 };
