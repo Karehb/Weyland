a3fd8b4f4c2a253d134d7a3946f14f95c7876e55
Karehb <Karehb@gmail.com>
2023-08-07 22:58:15 +0200

    imagenes de camaras 3 sec

--- config/software.js
+++ config/software.js
@@ -10,21 +10,41 @@ function camara(args) {
     if (!camaraDweet) {
         return `You do not have access to the camera with ID ${camaraId}`;
     }
-    return camaraDweet();
+
+    // Mostrar la cámara durante 3 segundos
+    const camaraContent = camaraDweet();
+    mostrarContenidoTemporal(camaraContent, 3000);
+
+    return null; // Opcionalmente, puedes retornar algo más útil aquí, o simplemente null.
+}
+
+function mostrarContenidoTemporal(contenido, tiempo) {
+    // Obtener el contenedor donde se mostrará el contenido temporalmente
+    const temporalContainer = document.createElement('div');
+    temporalContainer.innerHTML = contenido;
+
+    // Agregar el contenedor temporal al cuerpo del documento
+    document.body.appendChild(temporalContainer);
+
+    // Después de 'tiempo' milisegundos, borrar el contenido del contenedor y eliminarlo del documento
+    setTimeout(() => {
+        temporalContainer.innerHTML = '';
+        document.body.removeChild(temporalContainer);
+    }, tiempo);
 }
 
 const CAMERAS = {
-    888: function() {
-        return "<p>Video CAM-888</p><img src='config/cam1.jpg' class='glitch'>";
+    34: function() {
+        return "<p>Video CAM-34</p><img src='config/cam1.jpg' class='glitch'>";
     },
-    10549: function() {
-        return "<p>Video CAM-10549</p><img src='config/cam2.jpg' class='glitch'>";
+    888: function() {
+        return "<p>Video CAM-888</p><img src='config/cam2.jpg' class='glitch'>";
     },
     4800: function() {
         return "<p>Video CAM-4800</p><img src='config/cam3.jpg' class='glitch'>";
     },
-    34: function() {
-        return "<p>Video CAM-34</p><img src='config/cam4.jpg' class='glitch'>";
+    10549: function() {
+        return "<p>Video CAM-10549</p><img src='config/cam4.jpg' class='glitch'>";
     },
 }
 
