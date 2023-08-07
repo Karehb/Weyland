48548b998448910d6585fa54c89b4b5c10a38f51
Karehb <Karehb@gmail.com>
2023-08-07 03:20:15 +0200

    puertas puertas

--- config/software.js
+++ config/software.js
@@ -21,3 +21,30 @@ const CAMERAS = {
         return "<p>CAMARA DE SEGURIDAD 10549</p><img src='config/colonia_blue.jpg' class='glitch'>";
     },
 }
+
+function puerta(args) {
+  if (args.length === 0) {
+    const camerasList = Object.keys(PUERTAS).join("\n");
+    return [
+      "<p>An ID must be provided: <code>puerta $id</code></p>",
+      `You currently have access to the following doors:\n${camerasList}`
+    ];
+  }
+
+  const puertaId = args[0];
+  const puertaDweet = PUERTAS[puertaId];
+  if (!puertaDweet) {
+    return `You do not have access to the door with ID ${puertaId}`;
+  }
+
+  return puertaDweet();
+}
+
+const PUERTAS = {
+  1: function() {
+    return "<p>PUERTA 1</p><img src='config/door1.jpg' class='glitch'>";
+  },
+  2: function() {
+    return "<p>PUERTA 2</p><img src='config/door2.jpg' class='glitch'>";
+  },
+};
