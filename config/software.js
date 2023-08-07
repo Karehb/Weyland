a10d75f27f0cb507084348b78bc0ecbaba4951a3
Karehb <Karehb@gmail.com>
2023-08-07 03:04:39 +0200

    intentado fixear

--- config/software.js
+++ config/software.js
@@ -1,5 +1,5 @@
 function camara(args) {
-    if (args.length === 0 || typeof args[0] !== "number") {
+    if (args.length === 0) {
         return [
             "<p>An ID must be provided: <code>camara $id</code></p>",
             `You currently have access to the following cameras: ${Object.keys(CAMERAS).join(" ")}`
@@ -20,28 +20,4 @@ const CAMERAS = {
     10549: function() {
         return "<p>CAMARA DE SEGURIDAD 10549</p><img src='config/colonia_blue.jpg' class='glitch'>";
     },
-};
-
-function puerta(args) {
-    if (args.length === 0 || typeof args[0] !== "number") {
-        return [
-            "<p>An ID must be provided: <code>puerta $id</code></p>",
-            `You currently have access to the following puertas: ${Object.keys(PUERTAS).join(" ")}`
-        ];
-    }
-    const puertaId = args[0];
-    const puertaDweet = PUERTAS[puertaId];
-    if (!puertaDweet) {
-        return `You do not have access to the puerta with ID ${puertaId}`;
-    }
-    return puertaDweet();
 }
-
-const PUERTAS = {
-    777: function() {
-        return "<p>PUERTA DE SEGURIDAD 888</p>";
-    },
-    34908: function() {
-        return "<p>PUERTA DE SEGURIDAD 10549</p>";
-    },
-};
